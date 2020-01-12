b3acaad40cfc6e5e5d3fa83a5f6dd6bf1ea9f04d
Andwhit022 <59718154+Andwhit022@users.noreply.github.com>
2020-01-11 22:47:08 -0500

    Created array of objects with quotes

--- script.js
+++ script.js
@@ -10,7 +10,34 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+    {
+        quote:  'Failure is a feeling long before it becomes an actual result.',
+        source: 'Michelle Obama',
+        citation: 'Becoming',
+        year: 2018
+    },
+    {
+        quote:  'Teaching is the highest form of understanding',
+        source: 'Aristotle'
+    },
+    {
+        quote:  'The only way to know is to live, learn, and grow.',
+        source: 'Lauryn Hill'
+    },
+    {
+        quote:  'Without music, life would be a mistake.',
+        source: 'Friedrich Nietzsche',
+        citation: 'Twilight of the Idols',
+        year: 1889
+    },
+    {
+        quote:  "I'm the one that's got to die when it's time for me to die, so let me live my life the way I want to.",
+        source: 'Jimi Hendrix',
+        citation: 'Jimi Hendrix - Axis: Bold as Love',
+        year: 1989
+    }
+]
 
 
 /***
