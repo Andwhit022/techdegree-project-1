bd0bfe19bb74e90100d8688910d4024411b77473
Andwhit022 <59718154+Andwhit022@users.noreply.github.com>
2020-01-20 01:15:02 -0500

    added tag property

--- script.js
+++ script.js
@@ -12,45 +12,52 @@ project 1 - A Random Quote Generator
 //variable to hold output to the screen
 var message = '';
 
+
 //Array of objects to hold quote, source, citation, and year
 var quotes = [
     {
         quote:  'Failure is a feeling long before it becomes an actual result.',
         source: 'Michelle Obama',
         citation: 'Becoming',
-        year: 2018
+        year: 2018,
+        tags: "Politics"
     },
     {
         quote:  'People Say Nothing Is Impossible, But I Do Nothing Every Day',
         source: 'A.A Milne',
         citation: "Winne-the-Pooh",
-        year: ""
+        year: "",
+        tags: "Humor"
     },
     {
         quote:  'The only way to know is to live, learn, and grow.',
         source: 'Lauryn Hill',
         citation: "",
-        year: ""
+        year: "Music"
     },
     {
         quote:  'Without music, life would be a mistake.',
         source: 'Friedrich Nietzsche',
         citation: 'Twilight of the Idols',
-        year: 1889
+        year: 1889,
+        tags: "Philosophy"
     },
     {
         quote:  "I'm the one that's got to die when it's time for me to die, so let me live my life the way I want to.",
         source: 'Jimi Hendrix',
         citation: 'Jimi Hendrix - Axis: Bold as Love',
-        year: 1989
+        year: 1989,
+        tags: "Music"
     },
 ]
+
 
 //Function to target the div with the id 'quote-box'
 function print(){
     var outputDiv = document.getElementById('quote-box');
     outputDiv.innerHTML = message;
 }
+
 
 /***
  * `getRandomQuote` function to generate a random object from quote array and hold it in randomQuote variable
@@ -61,10 +68,12 @@ function getRandomQuote (){
     return randomQuote;
 }
 
+
 //added setInterval method to print a new quote every 25 seconds
 document.getElementById('load-quote').addEventListener("click", ()=>{
 setInterval(printQuote, 25000);
 });
+
 
 /***
  * `printQuote` function to print randomly selected object and print it to the screen
@@ -83,11 +92,17 @@ function printQuote(){
         } else {
             message += '';
         }
+        if (quotes.tags){
+            message += '<h3>' + quotes.tags + '</h3>';
+        } else {
+            message += '';
+        }
             message += '</p>';
                  
         print(message);
         
 }
+
 
 /***
  * click event listener for the print quote button
